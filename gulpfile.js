a78f0a1abfcf2eff8918c1fd73dcc59e57c20f2d
Clay Carpenter <claycarpenter@gmail.com>
2015-09-10 16:47:14 -0400

    Adds first working version of build system.

---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+// Import required dependencies.
+var gulp = require('gulp'),
+    browserSync = require('browser-sync'),
+    browserSyncReload = browserSync.reload,
+    sass = require('gulp-sass'),
+    filter = require('gulp-filter'),
+    concat = require('gulp-concat'),
+    del = require('del'),
+    runSequence = require('run-sequence');
+
+// Define project paths.
+// Note: all of these are relative to the project root.
+var projectPaths = {
+    sources: {
+        scss: 'src/scss/**/*.scss',
+        html: 'src/html/**/*.html',
+        images: 'src/images/**/*',
+        fonts: 'src/fonts/**/*',
+        scripts: 'src/js/**/*'
+    },
+    distRoot: 'dist',
+    testRoot: 'test',
+    testDistRoot: 'test/dist'
+};
+
+var browserSyncConfig = {
+    server: {
+        baseDir: './' + projectPaths.testRoot
+    },
+    files: [
+        // TODO: Can this be simplified?
+        projectPaths.testRoot + '/css/*.css',
+        projectPaths.testRoot + '/*.html',
+        projectPaths.testRoot + '/js/*.js'
+    ]
+};
+
+gulp.task('clean', function (cb) {
+  del([
+    // Clean out dist folder
+    projectPaths.distRoot,
+
+    // Clean out test/dist folder
+    projectPaths.testDistRoot
+  ], cb);
+});
+
+gulp.task('sass', function() {
+   return gulp.src(projectPaths.sources.scss)
+        .pipe(sass())
+        .pipe(gulp.dest(projectPaths.distRoot))
+        .pipe(filter('**/*.css'))
+        .pipe(browserSyncReload({stream: true}));
+});
+
+gulp.task('browser-sync', function() {
+    browserSync(browserSyncConfig);
+});
+
+gulp.task('copy-dist-test', function() {
+  gulp.src(projectPaths.distRoot + '/**/*')
+    .pipe(gulp.dest(projectPaths.testDistRoot));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(projectPaths.sources.scss, ['sass']);
+});
+
+gulp.task('build', ['sass']);
+
+gulp.task('clean-build', function (cb) {
+  runSequence('clean', 'build', cb);
+});
+
+gulp.task('test-build', function (cb) {
+  runSequence('clean-build', 'copy-dist-test', cb);
+});
+
+gulp.task('test-serve', function (cb) {
+  runSequence('test-build', 'watch', 'browser-sync', cb);
+});
+
+gulp.task('default', ['clean-build']);
